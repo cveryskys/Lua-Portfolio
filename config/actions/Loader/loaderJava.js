e81febb07d8309222598125a64b0118c344cfd7c
cveryskys <luvavden@gmail.com>
2025-02-18 13:08:50 -0600

    Intial commit

--- config/actions/Loader/loaderJava.js
+++ config/actions/Loader/loaderJava.js
@@ -4,16 +4,24 @@ window.addEventListener('load', () => {
   const container = document.querySelector('.container');
 
   setTimeout(() => {
-      heart.classList.add('break');
+      const crack = document.createElement('div');
+      crack.classList.add('crack');
+      heart.appendChild(crack);
+
       setTimeout(() => {
-          loader.classList.add('hidden');
+          heart.classList.add('fall-apart');
+          crack.classList.add('fall-apart-right');
+
           setTimeout(() => {
-              loader.style.display = 'none';
-              container.style.display = 'flex';
+              loader.classList.add('hidden');
               setTimeout(() => {
-                  container.style.opacity = 1;
-              }, 100); // Small delay to ensure display change is applied
-          }, 1000); // Duration of the fade-out and shrink animation
-      }, 1000); // Duration of the heart break animation
+                  loader.style.display = 'none';
+                  container.style.display = 'flex';
+                  setTimeout(() => {
+                      container.style.opacity = 1;
+                  }, 100); // Small delay to ensure display change is applied
+              }, 1000); // Duration of the fade-out and shrink animation
+          }, 1000); // Duration of the heart break animation
+      }, 1000); // Duration of the crack animation
   }, 5000); // Display loader for 5 seconds
 });
