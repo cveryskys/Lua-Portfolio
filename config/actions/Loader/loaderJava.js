456138ea8b9ff697818e65413cda8acbfd21578d
cveryskys <luvavden@gmail.com>
2025-02-18 12:54:00 -0600

    Lol

--- config/actions/Loader/loaderJava.js
+++ config/actions/Loader/loaderJava.js
@@ -1,15 +1,19 @@
 window.addEventListener('load', () => {
   const loader = document.querySelector('.loader');
+  const heart = document.querySelector('.heart');
   const container = document.querySelector('.container');
 
   setTimeout(() => {
-      loader.classList.add('hidden');
+      heart.classList.add('break');
       setTimeout(() => {
-          loader.style.display = 'none';
-          container.style.display = 'flex';
+          loader.classList.add('hidden');
           setTimeout(() => {
-              container.style.opacity = 1;
-          }, 100); // delay
-      }, 1000); // dur
-  }, 3000); // displayed
+              loader.style.display = 'none';
+              container.style.display = 'flex';
+              setTimeout(() => {
+                  container.style.opacity = 1;
+              }, 100); // Small delay to ensure display change is applied
+          }, 1000); // Duration of the fade-out and shrink animation
+      }, 1000); // Duration of the heart break animation
+  }, 5000); // Display loader for 5 seconds
 });
